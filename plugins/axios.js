277333b265e12da6af1e73c437e5892fdf4dd5c7
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2020-04-03 18:04:56 +0900

    LINEログイン/連携の実装 と 各種ページの実装

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,10 @@
 import { cacheAdapterEnhancer } from 'axios-extensions'
+// import Cookies from 'universal-cookie'
 import LRUCache from 'lru-cache'
 const ONE_HOUR = 1000 * 60 * 60
 const defaultCache = new LRUCache({ maxAge: ONE_HOUR })
 
-export default function ({ $axios }) {
+export default function ({ $axios, store }) {
   const defaults = $axios.defaults
   // https://github.com/kuitos/axios-extensions
   // https://github.com/nuxt-community/axios-module/issues/99#issuecomment-402320350
@@ -14,6 +15,9 @@ export default function ({ $axios }) {
   })
   $axios.onRequest((config) => {
     config.headers.common.Authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6Miwic2VxIjoxLCJwZXJtaXNzaW9uIjo5fQ.tLS3QO91IWv80TyH_KZTHo6wMSG2csAN0jQFWk4Zo2U'
+    if (store.$auth.loggedIn) {
+      config.headers.common.Authorization = 'Bearer ' + store.$auth.user.apiKey
+    }
     config.headers.common.Accept = 'application/json'
   })
 }
