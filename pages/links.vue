6723c7430769ef1beead9bb5a7d33ce6ea5ad1b2
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2020-04-25 19:55:50 +0900

    Web: Links ページの修正

--- pages/links.vue
+++ pages/links.vue
@@ -4,118 +4,82 @@
       <h1 class="title has-text-centered">
         リンク集
       </h1>
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child box">
+      <div class="columns is-multiline">
+        <div v-for="article in articles" :key="article.title" class="column is-6">
+          <div class="card">
             <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image">
-                    <img src="links/gochiusa.png" alt="Placeholder image">
-                  </figure>
+              <a class="has-text-dark" :href="'https://' + article.link">
+                <div class="container">
+                  <div class="columns is-mobile is-vcentered is-centered">
+                    <div class="column is-4-touch is-8-hd">
+                      <figure class="image has-image-centered">
+                        <img :src="article.img" :alt="article.title + 'logo image'">
+                      </figure>
+                    </div>
+                    <div class="column is-8-touch is-4-hd has-text-centered">
+                      <p class="title has-text-centered">
+                        <a class="has-text-dark" :href="'https://' + article.link">
+                          {{ article.title }}
+                        </a>
+                      </p>
+                      <p class="subtitle has-text-centered">
+                        <a class="has-text-dark" :href="'https://' + article.link">
+                          {{ article.link }}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="content columns is-vcentered is-centered">
+                    {{ article.description }}
+                  </div>
                 </div>
-                <div class="media-right">
-                  <p class="title">
-                    <a href="https://gochiusa.com">公式サイト</a>
-                  </p>
-                  <p class="subtitle">
-                    <a href="https://gochiusa.com">www.gochiusa.com</a>
-                  </p>
-                </div>
-              </div>
-              <br>
-              <div class="content columns is-vcentered is-centered">
-                とりあえずまず見に行ってください。ここを見たことない人は居ないと思いますが、とりあえずまず行ってください。
-                エイプリルフール、ココアの日、CD発売日など、様々なイベントのタイミングでサイトが変化します。
-                その時しか見られないものもあるので 定期的に監視をおすすめします。いつもご迷惑をおかけしております。
-              </div>
+              </a>
             </div>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image">
-                    <img src="links/pixiv.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="media-right">
-                  <p class="title">
-                    <a href="https://pixiv.net">Pixiv</a>
-                  </p>
-                  <p class="subtitle">
-                    <a href="https://pixiv.net">pixiv.net</a>
-                  </p>
-                </div>
-              </div>
-              <br>
-              <div class="content columns is-vcentered is-centered">
-                二次創作絵といえばだいたいここにある。人気順ソートが有料なのが少し残念に思っている。いつもご迷惑をおかけしております。
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image">
-                    <img src="links/twitter.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="media-right">
-                  <p class="title">
-                    <a href="https://twitter.com">Twitter</a>
-                  </p>
-                  <p class="subtitle">
-                    <a href="https://twitter.com">twitter.com</a>
-                  </p>
-                </div>
-              </div>
-              <br>
-              <div class="content columns is-vcentered is-centered">
-                絵師さんのメイン活動場所。最近はPixivよりもこっちのほうが流通する絵が多い。いつもご迷惑をおかけしております。
-              </div>
-            </div>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image">
-                    <img src="links/pawoo.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="media-right">
-                  <p class="title">
-                    <a href="https://pawoo.net">Pawoo</a>
-                  </p>
-                  <p class="subtitle">
-                    <a href="https://pawoo.net">pawoo.net</a>
-                  </p>
-                </div>
-              </div>
-              <br>
-              <div class="content columns is-vcentered is-centered">
-                PixivのMastodonインスタンス。色んな絵師さんがTwitterの避難所として利用している。いつもご迷惑をおかけしております。
-              </div>
-            </div>
-          </article>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 
+<style>
+.card {
+  height: 100%;
+}
+</style>
+
 <script>
 export default {
-  auth: false
+  auth: false,
+  data () {
+    return {
+      articles: [
+        {
+          title: '公式サイト',
+          link: 'gochiusa.com',
+          img: 'gochiusa.png',
+          description: 'とりあえずまず見に行ってください。ここを見たことない人は居ないと思いますが、とりあえずまず行ってください。エイプリルフール、ココアの日、CD発売日など、様々なイベントのタイミングでサイトが変化します。その時しか見られないものもあるので 定期的に監視をおすすめします。いつもご迷惑をおかけしております。'
+        },
+        {
+          title: 'ごちうさエイプリル',
+          link: 'april.gochiusa.team',
+          img: 'gochiusa.png',
+          description: 'ごちうさのエイプリルフール関連サイト群へのポータル(要ごちイラログイン)'
+        },
+        {
+          title: 'ごちうさイラストサイト開発部',
+          link: 'line.me/ti/g2/yBHVVHzN90s1r-DPlk9BFg',
+          img: 'square.png',
+          description: 'このサイトの開発進捗報告・ちょっとしたごちうさ情報共有などを行うグループ'
+        },
+        {
+          title: 'Mastodon',
+          link: 'pawoo.net',
+          img: 'pawoo.png',
+          description: 'PixivのMastodonインスタンス。色んな絵師さんがTwitterの避難所として利用している。いつもご迷惑をおかけしております。'
+        }
+      ]
+    }
+  }
 }
 </script>
