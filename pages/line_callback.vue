03083413f25f354f4856ca0c0d03124818658887
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2020-04-25 19:59:24 +0900

    Web: ログイン時にAprilにクッキーを焼くように

--- pages/line_callback.vue
+++ pages/line_callback.vue
@@ -26,9 +26,33 @@ export default {
         this.form.code = this.$route.query.code
         try {
           await this.$auth.loginWith('local', { data: this.form })
+          const cookies = {
+            name: 'gochiira_member',
+            value: 'true',
+            opts: {
+              path: '/',
+              domain: '',
+              maxAge: 60 * 60 * 24 * 7
+            }
+          }
+          const domains = [
+            '2014.gochiusa.team',
+            '2015.gochiusa.team',
+            '2016.gochiusa.team',
+            '2017.gochiusa.team',
+            '2018.gochiusa.team',
+            '2019.gochiusa.team',
+            '2020.gochiusa.team'
+          ]
+          const cookieList = []
+          domains.forEach((domain) => {
+            cookies.opts.domain = domain
+            cookieList.push(cookies)
+          })
+          this.$cookies.setAll(cookieList)
           this.$router.push({ path: '/' })
         } catch (error) {
-          this.$router.push({ path: '/login' })
+          this.$router.push({ path: '/login?err=1' })
         }
       } else {
         this.$router.push({ path: '/login' })
