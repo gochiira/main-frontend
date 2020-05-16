4107a31c83ff1b4e3aa0090cfba70dcdd4f6b6da
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-16 10:24:22 +0900

    Web: ヘルプやメニューやリンクなどの見た目改善

--- pages/help.vue
+++ pages/help.vue
@@ -1,62 +1,106 @@
 <template>
   <section class="section">
     <div class="container is-widescreen">
-      <h1 class="title has-text-centered">
-        ヘルプ
-      </h1>
-      <Accordion title="ごちイラとは?">
-        <p>
-          ごちイラはネット上に落ちている画像をユーザーおよび機械で収集して保存しているウェブクローンサイトです。 保存元で非公開になっても見ることができます。 サイトは無料で利用できます。自ら投稿もできます。本サイトは2020/04/YYより運営者・Omado(本名非公開)が運営しています。
-        </p>
-      </Accordion>
-      <Accordion title="ごちイラを見るのは違法じゃないの?">
-        <p>
-          違法ではありません。詳しくは<a href="http://www.bunka.go.jp/seisaku/chosakuken/hokaisei/h24_hokaisei/index.html">こちらを参照。</a>
-        </p>
-      </Accordion>
-      <Accordion title="ごちイラの使い方は?">
-        <p>
-          上部メニューから好きな条件を選び検索することで、各種イラストを探すことができます。
-          また、各イラストにいいねを押すことでいいね順ソートの検索に出やすくし、
-          他のユーザーに宣伝できます。詳しくはマイページ内の"ガイド"より。
-        </p>
-      </Accordion>
-      <Accordion title="ごちイラが重い、レイアウトが崩れる">
-        <p>
-          ごちイラは<a href="https://www.dell.com/ja-jp/shop/cty/pdp/spd/inspiron-15-7567-laptop?ref=PD_OC">Windows10/Chrome最新版</a>、
-          <a href="https://www.oppo.com/jp/smartphone-reno-a/">Android9/Chrome最新版</a>、
-          <a href="https://ja.wikipedia.org/wiki/IPhone_6s">iOS12/Chrome最新版</a>でのみ動作が確認されています。
-          <a href="https://time-space.kddi.com/digicul-column/suguyaru/20151221/">
-            インターネットエクスプローラ、ネットスケープなどの古代のブラウザ
-          </a>、
-          <a href="https://gigazine.net/news/20190815-fridge-tweet/">3DS、WiiU、冷蔵庫など</a>
-          では正常に表示されないと思います。
-          また<a href="https://ja.wikipedia.org/wiki/Tor">JavaScriptがオフのブラウザ</a>では表示できません。
-          大変恐れ入りますが、<a href="https://kakaku.com/pc/note-pc/guide_0020/">普通のコンピュータ</a>をご用意ください。
-        </p>
-      </Accordion>
-      <Accordion title="抜けがある・この画像が壊れてる・機能リクエスト">
-        <p>
-          <a href="https://forms.gle/uf3gYMfFSTNxqDuW8">
-            お問い合わせフォームからご連絡ください
-          </a>
-        </p>
-      </Accordion>
-      <Accordion title="権利者の方へ / DMCAリクエスト">
-        <p>
-          ごちイラは日本国内のいかなる企業・個人との関わりはありません。
-          また、画像の保管元ごちイラと一切関係ありません。
-          ごちイラは、神絵師さんをより布教すること、出所不明の<a href="https://note.com/free_yaken/n/n066acf4d6581">転載</a>を無くすこと、
-          この世のすべてのごちうさ絵を保存することを目的として完全無償で提供しているユーザー投稿型サイトです。
-          このサイト上に広告は一切存在せず、完全招待制であり、
-          かつGoogle等のあらゆる検索エンジンのインデックス(※検索結果に記載すること)をすべて拒否しています。
-          今後その予定はありませんし、絵師様の活動の邪魔をするつもりも全くありません。
-          しかし、それでも保存されると困る、このプロジェクトをどうしても受け入れられないという方は
-          <a href="https://forms.gle/JSG5rWkC94fJaGWbA">こちら</a>からご連絡ください。
-          可能な限り早く対応させていただきます。
-          皆様の神がかったごちうさ絵は日々崇めさせて頂いております、いつもありがとうございます。
-        </p>
-      </Accordion>
+      <div class="columns is-multiline is-centered">
+        <div class="column is-12">
+          <h1 class="title has-text-centered">
+            ヘルプ
+          </h1>
+        </div>
+        <div class="column is-8">
+          <Accordion title="ごちイラとは?" class="is-8">
+            <p>
+              ごちイラはネット上に落ちているごちうさ関連の画像を機械で収集し、保管するイラスト検索サイトです。サイトは完全無料で利用できます。自ら投稿もできます。本サイトは2020/04/22より運営者・Omado(本名非公開)が運営しています。
+            </p>
+          </Accordion>
+          <Accordion title="ごちイラの使い方は?">
+            <p>
+              上部メニューから好きな条件を選び検索することで、各種イラストを探すことができます。
+              また、各イラストにいいねを押すことでいいね順ソートの検索に出やすくし、
+              他のユーザーに宣伝できます。詳しくはマイページ内の"ガイド"より。
+            </p>
+          </Accordion>
+          <Accordion title="ごちイラが重い、レイアウトが崩れる">
+            <p>
+              ごちイラは<a href="https://www.dell.com/ja-jp/shop/cty/pdp/spd/inspiron-15-7567-laptop?ref=PD_OC">Windows10/Chrome最新版</a>、
+              <a href="https://www.oppo.com/jp/smartphone-reno-a/">Android9/Chrome最新版</a>、
+              <a href="https://ja.wikipedia.org/wiki/IPhone_6s">iOS12/Chrome最新版</a>でのみ動作が確認されています。
+              <a href="https://time-space.kddi.com/digicul-column/suguyaru/20151221/">
+                インターネットエクスプローラ、ネットスケープなどの古代のブラウザ
+              </a>、
+              <a href="https://gigazine.net/news/20190815-fridge-tweet/">3DS、WiiU、冷蔵庫など</a>
+              では正常に表示されないと思います。
+              また<a href="https://ja.wikipedia.org/wiki/Tor">JavaScriptがオフのブラウザ</a>では表示できません。
+              大変恐れ入りますが、<a href="https://kakaku.com/pc/note-pc/guide_0020/">普通のコンピュータ</a>をご用意ください。
+            </p>
+          </Accordion>
+          <Accordion title="ごちイラの運営は違法じゃないの?">
+            <p class="has-text-weight-bold">
+              当サイトは検索情報の特定又は所在に関する情報を検索し，及びその結果を提供するもの(著作権法47条の5)にあたるため違法ではありません。
+            </p>
+            <br>
+            <p>
+              また、当サイトはWebサイト開発技術の研究を目的としており、その中で利用される著作物であるイラストは機械学習技術の向上を目的として公開しています。これは、技術の開発等のための試験の用に供する利用(著作権法第30条の4)でもあるため、やはり違法となりえません。
+            </p>
+            <br>
+            <p class="has-text-weight-bold">
+              当サイトを構成するイラスト・説明文・素材・商標等にかかわる知的財産権等の権利は、すべて各作者様/投稿者様に帰属します。
+            </p>
+            <p>
+              (平成30年(2018年)の著作権法の改正後時点の条文より)
+            </p>
+          </Accordion>
+          <Accordion title="ごちイラの閲覧は違法じゃないの?">
+            <p class="has-text-weight-bold">
+              ごちイラは法律に則り運営していますが、万が一問題があったとしても、
+              下記の場合においても、ユーザーは適法に利用できます。
+            </p>
+            <br>
+            <p>
+              <b>[ごちイラ内のコンテンツが違法に配信されていた場合]</b>
+            </p>
+            <p>
+              違法ダウンロードの刑事罰化におけるダウンロードとは、
+              インターネット上から自分のパソコンやスマホなどにファイルをコピー（複製）することであり、
+              閲覧すること自体は違法にあたりません。
+              ダウンロードした後に違法配信と知った場合は、違法とはなりません。また刑事罰の対象にもなりません。
+              (平成24年7月 違法ダウンロードの刑事罰化についてのQ&Aより)
+            </p>
+            <br>
+            <p>
+              <b>[ごちイラ内の違法に配信されているコンテンツへのリンクの記載]</b>
+            </p>
+            <p>
+              無断でアップロードされていることが明らかではない著作物についてユーザーがリンクを貼っても直ちに違法にはならず、著作権者から抗議を受ける等無断にアップロードされた著作物であることを認識できた時点でリンクを削除すれば不法行為となりません。
+              (動画サイトへのリンクが著作権侵害に当たるのかが争われた事例 大阪地方裁判所 平成25年6月20日の判決より)
+            </p>
+          </Accordion>
+          <Accordion title="ごちイラのデータをダウンロードしてもいいの?">
+            <ul>
+              <li>私的使用のための複製（著作権法第30条）</li>
+              <li>引用（著作権法第32条）</li>
+              <li>送信可能化された情報の送信元識別符号の検索等のための複製等（著作権法第47条の5）</li>
+            </ul>
+            を目的としている場合は問題ありません。
+          </Accordion>
+          <Accordion title="抜けがある・この画像が壊れてる・機能リクエスト">
+            <p>
+              <a href="https://forms.gle/uf3gYMfFSTNxqDuW8">
+                お問い合わせフォームからご連絡ください
+              </a>
+            </p>
+          </Accordion>
+          <Accordion title="権利者の方へ / DMCAリクエスト">
+            <p>
+              ごちイラは 日本国内の法律に則り運営されています。
+              ごちイラ内に記載してほしくない、または違法コンテンツが記載されている場合は
+              権利者の方が直接、<a href="https://forms.gle/JSG5rWkC94fJaGWbA">こちら</a>のフォームよりご連絡ください。
+              フォーム内容が少々複雑ではありますが、虚偽申請を防ぐための措置です。
+              お手数おかけしますがご協力お願いします。
+            </p>
+          </Accordion>
+        </div>
+      </div>
     </div>
   </section>
 </template>
--- pages/links.vue
+++ pages/links.vue
@@ -7,34 +7,36 @@
       <div class="columns is-multiline">
         <div v-for="article in articles" :key="article.title" class="column is-6">
           <div class="card">
-            <div class="card-content">
-              <a class="has-text-dark" :href="'https://' + article.link">
-                <div class="container">
-                  <div class="columns is-mobile is-vcentered is-centered">
-                    <div class="column is-4-touch is-8-hd">
-                      <figure class="image has-image-centered">
-                        <img :src="article.img" :alt="article.title + 'logo image'" decoding="async">
-                      </figure>
+            <a :href="'https://' + article.link" rel="noopener noreferrer nofollow">
+              <div class="card-content">
+                <a class="has-text-dark">
+                  <div class="container">
+                    <div class="columns is-mobile is-vcentered is-centered">
+                      <div class="column is-6-touch is-8-hd">
+                        <figure class="image is-128x128 has-image-centered">
+                          <img :src="'../links/'+article.img" :alt="article.title + 'logo image'" decoding="async">
+                        </figure>
+                      </div>
+                      <div class="column is-6-touch is-4-hd has-text-centered">
+                        <p class="title has-text-centered">
+                          <a class="has-text-dark">
+                            {{ article.title }}
+                          </a>
+                        </p>
+                        <p class="subtitle has-text-centered">
+                          <a class="has-text-dark">
+                            {{ article.link }}
+                          </a>
+                        </p>
+                      </div>
                     </div>
-                    <div class="column is-8-touch is-4-hd has-text-centered">
-                      <p class="title has-text-centered">
-                        <a class="has-text-dark" :href="'https://' + article.link">
-                          {{ article.title }}
-                        </a>
-                      </p>
-                      <p class="subtitle has-text-centered">
-                        <a class="has-text-dark" :href="'https://' + article.link">
-                          {{ article.link }}
-                        </a>
-                      </p>
+                    <div class="content columns is-vcentered is-centered">
+                      {{ article.description }}
                     </div>
                   </div>
-                  <div class="content columns is-vcentered is-centered">
-                    {{ article.description }}
-                  </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
+            </a>
           </div>
         </div>
       </div>
@@ -56,26 +58,26 @@ export default {
         {
           title: '公式サイト',
           link: 'gochiusa.com',
-          img: 'gochiusa.png',
-          description: 'とりあえずまず見に行ってください。ここを見たことない人は居ないと思いますが、とりあえずまず行ってください。エイプリルフール、ココアの日、CD発売日など、様々なイベントのタイミングでサイトが変化します。その時しか見られないものもあるので 定期的に監視をおすすめします。いつもご迷惑をおかけしております。'
+          img: 'animal_usagi_nihon_nousagi.png',
+          description: 'いつもの。やっぱこれだね。親の顔より見た顔。実家のような安心感。'
+        },
+        {
+          title: 'ごちイラ開発部',
+          link: 'line.me/ti/g2/yBHVVHzN90s1r-DPlk9BFg',
+          img: 'computer_girl.png',
+          description: 'このサイトの開発進捗報告、ちょっとしたごちうさ情報共有などを行うグループ'
         },
         {
           title: 'ごちうさエイプリル',
           link: 'april.gochiusa.team',
-          img: 'gochiusa.png',
-          description: 'ごちうさのエイプリルフール関連サイト群へのポータル(要ごちイラログイン)'
+          img: 'aprilfool_calender.png',
+          description: 'ごちうさのエイプリルフール関連サイトへのポータル'
         },
         {
-          title: 'ごちうさイラストサイト開発部',
-          link: 'line.me/ti/g2/yBHVVHzN90s1r-DPlk9BFg',
-          img: 'square.png',
-          description: 'このサイトの開発進捗報告・ちょっとしたごちうさ情報共有などを行うグループ'
-        },
-        {
-          title: 'Mastodon',
-          link: 'pawoo.net',
-          img: 'pawoo.png',
-          description: 'PixivのMastodonインスタンス。色んな絵師さんがTwitterの避難所として利用している。いつもご迷惑をおかけしております。'
+          title: 'ごちうさコミュ',
+          link: 'illust.gochiusa.team',
+          img: 'figure_group_colorful.png',
+          description: 'ごちうさファンコミュニティへのポータル(そのうち作成)'
         }
       ]
     }
