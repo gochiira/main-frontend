51a0bc3aba28f1ec215c74c9c736f1d867e1ed9e
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-09-16 18:58:30 +0900

    Web: Notificationコンポーネントの仕様変更(Slot化)

--- pages/history/illust.vue
+++ pages/history/illust.vue
@@ -3,10 +3,9 @@
     <div class="container is-widescreen">
       <div class="columns is-centered is-vcentered is-multiline">
         <div class="column is-8">
-          <Notification
-            icon="info"
-            :title="$auth.user.name+' さんのイラスト閲覧履歴 (' + $store.state.user.illustHistory.length + '件)'"
-          />
+          <Notification icon="info">
+            {{ $auth.user.name+' さんのイラスト閲覧履歴 (' + $store.state.user.illustHistory.length + '件)' }}
+          </Notification>
         </div>
       </div>
       <div class="columns is-centered is-multiline is-mobile">
